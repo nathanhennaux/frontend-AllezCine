c64434ff57f1479f389acceb41b23474f53d0a6b
Jonathan <jonathan.fauchoux@gmail.com>
2018-11-05 21:29:11 +0100

    init

---
+++ assets/js/anime.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".btnAction").click(function(){
+        $(".sildeAction").fadeToggle();
+       
+       
+    });
+     $(".btnFantastique").click(function(){
+        
+        $(".sildeFantastique").fadeToggle("slow");
+        
+    });
+});

95fe65472e5fdc0d57c198c77a3756e946c55553
Jonathan <jonathan.fauchoux@gmail.com>
2018-11-06 15:34:14 +0100

    Featured movies

--- assets/js/anime.js
+++ assets/js/anime.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
-    $(".btnAction").click(function(){
-        $(".sildeAction").fadeToggle();
-       
-       
+    $(".btnAll").click(function(){
+        $(".slidePolicier").fadeToggle('slow'); 
+        $(".slideAction").fadeToggle("slow");   
     });
-     $(".btnFantastique").click(function(){
-        
-        $(".sildeFantastique").fadeToggle("slow");
-        
+    $(".btnAction").click(function(){   
+        $(".slideAction").fadeToggle("slow");    
+    });
+    $(".btnPolicier").click(function(){   
+        $(".slidePolicier").fadeToggle("slow");    
+    });
+    $(".btnPlus").click(function(){   
+        $(".slidePolicier2").fadeToggle("slow");
+        $(".slideAction2").fadeToggle("slow");    
     });
 });
